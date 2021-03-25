35cfcaaa02e7be4687d97ef4d419ba03190577cb
Mauro Sanchez <novamauro98@gmail.com>
2021-03-25 16:37:05 -0500

    Finished the login form for the frontend. Still need to work on the css.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,26 @@
-function App() {
+import { useDispatch } from 'react-redux';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from './store/session';
+
+
+const App = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const handClick = () => {
+    dispatch(sessionActions.logoutUser())
+  }
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route exact path="/">
+        <h1>Hello from App</h1>
+        <button onClick={handClick}>Log Out</button>
+        <button onClick={() => history.push('/login')}>Log In</button>
+      </Route>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
