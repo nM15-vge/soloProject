1a306d921f06e9f4e395bf5aa01bf71803625e7c
Mauro Sanchez <novamauro98@gmail.com>
2021-03-24 20:35:31 -0500

    Finished the setup for database and express app on backend.

---
+++ backend/config/database.js
@@ -0,0 +1,28 @@
+const {
+  username,
+  password,
+  database,
+  host
+} = require('./index').db;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
