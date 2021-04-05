c29970cffdf8a7cd16a80c8472998643d77e18bc
Mauro Sanchez <novamauro98@gmail.com>
2021-04-05 15:24:53 -0500

    Changes to upload.

--- frontend/src/components/Uploads/index.js
+++ frontend/src/components/Uploads/index.js
@@ -5,11 +5,10 @@ export const fileObj = {
   type: "image/png"
 }
 
-export const options = {
-  keyPrefix: "uploads/",
-  bucket: "throughlens",
-  region: "us-east-2",
-  accessKey: "AKIA46HPUQANYI4AWEXK",
-  secretKey: "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",
-  successActionStatus: 201
-}
+// export const options = {
+//   keyPrefix: "uploads/",
+//   bucket: "throughlens",
+//   region: "us-east-2",
+//   accessKey:
+//   successActionStatus: 201
+// }
