5a66cb3cc0519c8c2eba4dab9ebf93bcb307560c
Mauro Sanchez <novamauro98@gmail.com>
2021-06-09 11:08:45 -0500

    Working on the modal form for a single picture upload.

--- frontend/src/components/ImagesUpload/index.js
+++ frontend/src/components/ImagesUpload/index.js
@@ -1,10 +1,16 @@
 import { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { Modal } from '../../context/Modal';
 import styles from './ImageUpload.module.css';
 
 const ImagesUpload = () => {
   const [image, setImage] = useState({});
-  const [imageUrl, setImageUrl] = useState({})
+  const [imageUrl, setImageUrl] = useState(null);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [publicPrivate, setPublicPrivate] = useState(false);
+  const [albumId, setAlbumId] = useState(0);
+
   const onDrop = useCallback(acceptedFiles => {
     acceptedFiles.forEach(file => {
       const reader = new FileReader();
@@ -24,10 +30,46 @@ const ImagesUpload = () => {
   if(image){
     console.log(image);
   };
+
+  const onSubmit = () => {
+
+  }
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
   return (
     <>
-      <div>{imageUrl &&  <img key={imageUrl.dataUrl} src={imageUrl.dataUrl} alt={imageUrl.name} style={{"width": "150px", "height": "150px"}}></img>}</div>
+      <div>{imageUrl && (
+        <Modal onClose={() => setImageUrl(null)}>
+          <img key={imageUrl.dataUrl} src={imageUrl.dataUrl} alt={imageUrl.name} style={{"width": "150px", "height": "150px"}} />
+          <form onSubmit={onSubmit}>
+            <label>
+                Title:
+                <input type="text" onChange={e => setTitle(e.target.value)} value={title}/>
+            </label>
+            <label>
+              Description:
+              <textarea onChange={e => setDescription(e.target.value)} value={description}/>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="publicPrivate"
+                onChange={() => setPublicPrivate(true)}
+                checked={publicPrivate ? true: false}
+              />
+              Public
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="publicPrivate"
+                onChange={() => setPublicPrivate(false)}
+                checked={!publicPrivate? true: false}
+              />
+              Private
+            </label>
+          </form>
+        </Modal>
+      )}</div>
       <div className={styles.dropzone} {...getRootProps()}>
         <input {...getInputProps()} />
         {
